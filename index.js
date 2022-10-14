294cc90ad703b71ddf5e98bf3a19fcffe20b04f6
parykhan-jameel <parexan.00443431@gmail.com>
2022-10-15 00:56:27 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const thankYouMessages = []
+    for (let n = 0; n < names.length; n++) {
+        let message = `Thank you, ${names[n]}, for the wonderful ${event} gift!`
+        thankYouMessages.push(message)
+    };
+    return thankYouMessages
+};
+
+
+function countDown(int) {
+    while (int > -1) {
+        console.log(int)
+        int--;
+    }
+}
